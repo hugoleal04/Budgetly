a4849169ec868a41ec9724a3ad17ee8dde741281
Hugo Leal <hugo.leal7755@gmail.com>
2025-09-28 13:03:09 +0100

    Add features to the generated pdf
    Now you can see how much do you spend on a month and the avg of how much you spend per day

--- obj/Debug/net8.0/Budgetly.AssemblyInfo.cs
+++ obj/Debug/net8.0/Budgetly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Budgetly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e168f06809007342d25054ac4ebcaa23fa075c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499fd07d805f582cc707d519f9c8232809bc01ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Budgetly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Budgetly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
